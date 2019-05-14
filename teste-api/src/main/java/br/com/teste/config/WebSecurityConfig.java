c2172fbff0e8dde23fa8d2f23032082f8950c252
Luiz Renato <luizrenato2005@gmail.com>
2019-05-14 00:25:43 -0300

    conclusao autenticação basic

--- teste-api/src/main/java/br/com/teste/config/WebSecurityConfig.java
+++ teste-api/src/main/java/br/com/teste/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package br.com.teste.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,14 +15,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("ROLE");
+			.withUser("user").password("teste").roles("ROLE");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.authorizeRequests()
+			.antMatchers("/api/produto").permitAll()
+			.anyRequest().authenticated().and()
+			.httpBasic().and()
+			.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+
+	
 
 	
 }
